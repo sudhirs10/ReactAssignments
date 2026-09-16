de7ebbc42ec306942165fd92d29a2ad4d9aaafcc
sudhirs10 <Sudhir.Shrestha2@metropolia.fi>
2026-09-16 14:11:23 +0545

    Complete first React components exercise

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import {defineConfig} from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: {js},
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
 ]);
